128433c61a06b66f36e5102b52b11c99a3f9cabe
garbog2 <39171249+garbog2@users.noreply.github.com>
2018-09-03 21:56:57 -0400

    Update 7. Finding a motif in DNA.py

--- 7. Finding a motif in DNA.py
+++ 7. Finding a motif in DNA.py
@@ -1,5 +1,6 @@
 import regex as re
-### upgraded regex provides the overlapped = True statement, and the ',' after print prints everything on one line ###
+### upgraded regex provides the overlapped = True statement, and the ',' after print prints everything on one line. For some reason, Rosalind 
+### uses 1-based indexing, which the '+1' is for at the end
 def find_dna_motif_re(dna, motif):
     for m in re.finditer(motif, dna, overlapped = True):
         print m.start() + 1,
